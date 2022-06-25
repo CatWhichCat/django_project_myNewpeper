bab84f91a83d83209b03bb55c0bc326947b32c22
CatWhichCat <fortunalich@mail.ru>
2022-06-26 02:00:14 +0600

    error

--- news/forms.py
+++ news/forms.py
@@ -1,7 +1,26 @@
 from .models import Articles
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
 class ArticlesForm(ModelForm):
     class Meta:
         model = Articles
         fields = ['title', 'anons', 'full_text', 'date']
+        
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'название статьи',
+                }),
+            "anons": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Анонс статьи'
+            }),
+            "date": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Дата и время'
+            }),
+            "full_text": DateTimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Текс статьи'
+            })
+        }
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import ArticlesForm
 
@@ -9,8 +9,20 @@ def news_home(request):
 
 
 def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect( 'home' )
+        else:
+            error = 'Форма была неверной'
+    
     form = ArticlesForm()
 
-    context = {'form': form}
+    context = {
+        'form': form,
+        'error': error
+        }
 
     return render(request, 'news/create.html', context)
